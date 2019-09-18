638ae07fe53b62b974b3ae37f240db8c77b67b37
Trivernis <trivernis@hotmail.de>
2019-09-18 10:51:48 +0200

    Code cleanup, Version correction
    - changed version from 0.10-beta-SNAPSHOT to 0.10-beta

--- src/main/kotlin/net/trivernis/chunkmaster/lib/generation/TaskEntry.kt
+++ src/main/kotlin/net/trivernis/chunkmaster/lib/generation/TaskEntry.kt
@@ -2,5 +2,4 @@ package net.trivernis.chunkmaster.lib.generation
 
 import org.bukkit.scheduler.BukkitTask
 
-data class TaskEntry(val id: Int, val task: BukkitTask, val generationTask: GenerationTask) {
-}
+data class TaskEntry(val id: Int, val task: BukkitTask, val generationTask: GenerationTask)
