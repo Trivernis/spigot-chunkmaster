992f4e4cd344fcd2ec14a5fb8dd51bbd4a3f9f4a
trivernis <trivernis@protonmail.com>
2020-07-26 16:47:33 +0200

    Add locking to ChunkUnloader class

--- src/main/kotlin/net/trivernis/chunkmaster/lib/generation/ChunkUnloader.kt
+++ src/main/kotlin/net/trivernis/chunkmaster/lib/generation/ChunkUnloader.kt
@@ -5,42 +5,56 @@ import org.bukkit.Chunk
 import java.lang.Exception
 import java.util.*
 import java.util.concurrent.*
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.collections.HashSet
 
 class ChunkUnloader(private val plugin: Chunkmaster): Runnable {
     private val maxLoadedChunks = plugin.config.getInt("generation.max-loaded-chunks")
+    private val lock = ReentrantReadWriteLock()
     private var unloadingQueue = Vector<Chunk>(maxLoadedChunks)
     val isFull: Boolean
-    get() {
-        return unloadingQueue.size == maxLoadedChunks
-    }
+        get() {
+            return pendingSize == maxLoadedChunks
+        }
 
     val pendingSize: Int
         get() {
-            return unloadingQueue.size
+            lock.readLock().lock()
+            val size = unloadingQueue.size
+            lock.readLock().unlock()
+            return size
         }
 
     /**
      * Unloads all chunks in the unloading queue with each run
      */
     override fun run() {
-        val chunkToUnload = unloadingQueue.toHashSet()
-        unloadingQueue.clear()
+        lock.writeLock().lock()
+        try {
+            val chunkToUnload = unloadingQueue.toHashSet()
 
-        for (chunk in chunkToUnload) {
-            try {
-                chunk.unload(true)
-            } catch (e: Exception) {
-                plugin.logger.severe(e.toString())
+            for (chunk in chunkToUnload) {
+                try {
+                    chunk.unload(true)
+                } catch (e: Exception) {
+                    plugin.logger.severe(e.toString())
+                }
             }
+            unloadingQueue.clear()
+        } finally {
+            lock.writeLock().unlock()
         }
-        unloadingQueue.clear()
     }
 
     /**
      * Adds a chunk to unload to the queue
      */
     fun add(chunk: Chunk) {
-        unloadingQueue.add(chunk)
+        lock.writeLock().lockInterruptibly()
+        try {
+            unloadingQueue.add(chunk)
+        } finally {
+            lock.writeLock().unlock()
+        }
     }
 }
